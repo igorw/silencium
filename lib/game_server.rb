f107bbea79f5a892826acce62e5a292604c6be25
Igor Wiedler <igor@wiedler.ch>
2010-07-10 02:24:50 +0200

    move game and room servers to separate files

---
+++ lib/game_server.rb
@@ -0,0 +1,256 @@
+module Silencium
+  class GameServer
+    include ServerBase
+    
+    def initialize(name, port, cards)
+      @name = name
+      @port = port
+    
+      @cards = cards
+      @old_cards = []
+      @users = []
+      @card = nil
+    
+      @time_remaining = 60
+      @paused = true
+      @game_over = false
+    
+      EM::PeriodicTimer.new(1) do
+        if !@paused
+          if @time_remaining == 0
+            @paused = true
+            next_round
+          end
+        
+          @time_remaining -= 1
+        
+          trigger_time_sync
+        end
+      end
+    end
+  
+    def reset
+      @cards = @old_cards
+      @old_cards = []
+      @card = nil
+    
+      @time_remaining = 60
+      @paused = true
+      @game_over = false
+    
+      trigger_global_event Event.new(:reset)
+    
+      @users = []
+    end
+  
+    def init_mq(queue, options)
+      super(queue, options)
+    
+      # contact room server
+      EM::PeriodicTimer.new(5) do
+        trigger_mq_event Event.new(:room_broadcast, {name: @name, port: @port, users: @users.size})
+      end
+    end
+  
+    def client_connect(ws, sid)
+      log "Client connected: #{sid}"
+    
+      trigger_event ws, Event.new(:connect)
+    end
+  
+    def client_disconnect(ws, sid)
+      log "Client disconnected: #{sid}"
+    
+      remove_user ws
+      user_count_changed :leave
+    end
+    
+    def trigger_global_event(event)
+      super(event)
+    
+      log "Trigger global event: " + event.to_s
+    end
+  
+    def receive_event(ws, event)
+      log "Received event: #{event.to_s}"
+    
+      user = find_user(ws)
+    
+      if event.name != :join && user.nil?
+        log "Rejected #{event.name.to_s} event from non-joined user (ws: #{ws})"
+        return
+      elsif event.name == :guess && is_giver?(user)
+        log "Giver trying to guess"
+        return
+      end
+    
+      case event.name
+        when :join then
+          error = false
+          if event.data[:username].empty?
+            error = "No username given"
+          elsif find_user_name(event.data[:username])
+            error = "Username already taken"
+          end
+        
+          if error
+            trigger_event ws, Event.new(:join, {accepted: false, username: event.data[:username], message: error})
+          else
+            @users << User.new(ws, event.data[:username])
+            trigger_event ws, Event.new(:join, {accepted: true, username: event.data[:username]})
+            trigger_event ws, Event.new(:debug, message: "joined game")
+            user_count_changed :join
+          end
+        when :guess then
+          correct = false
+        
+          # correct guess
+          if event.data[:word] == @card.word
+            user.score += 1
+            find_giver.score += 1
+            correct = true
+          
+            next_card
+          end
+        
+          trigger_global_event Event.new(:guess, {username: user.name, word: event.data[:word], correct: correct})
+        when :give then
+          hint = event.data[:hint]
+        
+          regex = Regexp.new(Regexp.escape(@card.word) + '|' + @card.taboo_words.map { |word| Regexp.escape(word) }.join('|'))
+        
+          if regex.match(hint)
+            find_giver.trigger_event Event.new(:system_message, message: "#{hint} is taboo, you fool")
+          else
+            trigger_global_event Event.new(:give, {username: user.name, hint: hint})
+          end
+      end
+    end
+    
+    def receive_mq_event(event)
+    end
+  
+    # called whenever a user joins or leaves
+    # status is :join or :leave
+    def user_count_changed(status = :join)
+      # set up giver
+      if @users.size > 0
+        @card = @cards.first
+      
+        if @card.nil?
+          game_over
+          return
+        end
+      
+        giver = find_giver
+        giver.trigger_event Event.new(:become_giver)
+        giver.trigger_event Event.new(:new_card, {word: @card.word, taboo_words: @card.taboo_words})
+      end
+    
+      # check if more than one user is playing
+      if @users.size == 1
+        # only one guy left
+        @paused = true
+        trigger_global_event Event.new(:pause)
+      elsif @users.size == 2 && status == :join
+        # second user joined, game can continue
+        @paused = false
+        trigger_global_event Event.new(:unpause)
+      end
+    
+      trigger_users
+      trigger_time_sync
+    end
+  
+    def find_user(ws)
+      @users.each do |user|
+        if ws === user.ws
+          return user
+        end
+      end
+    end
+  
+    def find_user_name(name)
+      @users.each do |user|
+        if name === user.name
+          return user
+        end
+      end
+    
+      nil
+    end
+  
+    def remove_user(ws)
+      user = find_user(ws)
+      @users.delete user
+    end
+  
+    def find_giver
+      @users.first
+    end
+  
+    def is_giver?(user)
+      user === find_giver
+    end
+  
+    def next_round
+      log "Next round"
+    
+      # first user becomes last
+      @users << @users.shift
+    
+      find_giver.trigger_event Event.new(:become_giver)
+      @users.last.trigger_event Event.new(:become_player)
+    
+      @time_remaining = 60
+      @paused = false
+    
+      next_card
+    end
+  
+    def next_card
+      log "Next card"
+    
+      @old_cards << @cards.shift
+      @card = @cards.first
+    
+      if @card.nil?
+        game_over
+        return
+      end
+    
+      find_giver.trigger_event Event.new(:new_card, {word: @card.word, taboo_words: @card.taboo_words})
+      trigger_users
+    end
+  
+    def game_over
+      log "Game over"
+    
+      if !@game_over
+        EM::Timer.new(10) do
+          reset
+        end
+      end
+    
+      @paused = true
+      @game_over = true
+    
+      scoreboard = @users.sort {|a, b| a.score <=> b.score }.map { |user| {name: user.name, score: user.score} }.reverse
+      trigger_global_event Event.new(:game_over, users: scoreboard)
+    end
+  
+    def trigger_time_sync
+      trigger_global_event Event.new(:time_sync, time_remaining: @time_remaining)
+    end
+  
+    def trigger_users
+      trigger_global_event Event.new(:users, users: @users.map { |user| {name: user.name, giver: is_giver?(user), score: user.score} })
+    end
+    
+    def trigger_global_event(event)
+      @users.each do |user|
+        user.trigger_event event
+      end
+    end
+  end
+end
--- rooms.rb
+++ rooms.rb
@@ -9,72 +9,11 @@ require 'json'
 require 'warren'
 require 'warren/adapters/amqp_adapter'
 
-require './lib/server_base.rb'
-
 require './model/event.rb'
 require './model/room.rb'
 
-module Silencium
-  class RoomServer
-    include ServerBase
-    
-    def initialize
-      @rooms = {}
-    
-      EM::PeriodicTimer.new(10) do
-        trigger_global_event rooms_event
-      end
-    
-      EM::PeriodicTimer.new(3) do
-        clean
-      end
-    end
-  
-    def client_connect(ws, sid)
-      trigger_event ws, rooms_event
-    
-      log "Client connected: #{sid}"
-    end
-  
-    def client_disconnect(ws, sid)
-      log "Client disconnected: #{sid}"
-    end
-  
-    def trigger_global_event(event)
-      @ws_channel.push event
-    
-      log "Trigger global event: " + event.to_s
-    end
-  
-    def receive_mq_event(event)
-      log "Reveived mq event: #{event.to_s}"
-    
-      case event.name
-        when :room_broadcast then
-          if @rooms[event.data[:name]].nil?
-            @rooms[event.data[:name]] = Room.new(event.data[:name], event.data[:port], event.data[:users])
-          else
-            @rooms[event.data[:name]].alive
-            @rooms[event.data[:name]].update(event.data[:users])
-          end
-      end
-    end
-  
-    # remove dead rooms
-    # rooms must broadcast all 10 seconds
-    def clean
-      @rooms.values.each do |room|
-        if room.updated < Time.new - 10
-          @rooms.delete room.name
-        end
-      end
-    end
-  
-    def rooms_event
-      Event.new(:rooms, rooms: @rooms.values.map { |room| {name: room.name, port: room.port, users: room.users} })
-    end
-  end
-end
+require './lib/server_base.rb'
+require './lib/room_server.rb'
 
 EM.run {
   @server = Silencium::RoomServer.new
